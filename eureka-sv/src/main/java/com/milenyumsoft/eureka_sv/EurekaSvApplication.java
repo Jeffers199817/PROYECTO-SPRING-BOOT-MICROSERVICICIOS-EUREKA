8aa1200390a60d101f9cda1bf2bbf2d8bff14796
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-03-05 22:39:29 -0500

    Final de lproyecto

--- eureka-sv/src/main/java/com/milenyumsoft/eureka_sv/EurekaSvApplication.java
+++ eureka-sv/src/main/java/com/milenyumsoft/eureka_sv/EurekaSvApplication.java
@@ -3,6 +3,7 @@ package com.milenyumsoft.eureka_sv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,6 +11,8 @@ public class EurekaSvApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaSvApplication.class, args);
+
+		System.out.println("Inicio de poryecto");
 	}
 
 }
